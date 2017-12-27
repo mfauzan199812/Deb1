fd046e417dd9412afb82574ed86b061b59474aef
Suro Diwiryo <serverkencang@gmail.com>
2017-12-27 17:44:12 +0700

    Create tendang.sh

---
+++ conf/tendang.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+# cek ip
+vps="adityawg";
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+wget -q -O adit http://skrip.kerdunet.info/notfound.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	else
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
+
+echo -e "----------------- Menendang User Yg Multi Login --------------------"
+read -p "Berapa Multi Login Yg Akan Ditendang (1-2):" limit
+userlimit $limit
