6a37b766f25bd2d634585da453dab5cc626e0f4f
Suro Diwiryo <serverkencang@gmail.com>
2017-12-27 19:31:49 +0700

    Update lock.sh

--- conf/lock.sh
+++ conf/lock.sh
@@ -4,7 +4,24 @@ if [[ $USER != 'root' ]]; then
 	echo "Maaf, Anda harus menjalankan ini sebagai root"
 	exit
 fi
-
+# cek ip
+vps="adityawg";
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+wget -q -O adit https://raw.githubusercontent.com/DomeiNokiO/xsped/master/adt.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	else
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
 
 echo "" > /root/infouser.txt
 echo "" > /root/expireduser.txt
