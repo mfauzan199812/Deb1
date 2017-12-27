aa8eb8b358e360e23f5ac7525c2620b7fbb75a30
Suro Diwiryo <serverkencang@gmail.com>
2017-12-27 17:52:25 +0700

    Update generate.sh

--- conf/generate.sh
+++ conf/generate.sh
@@ -4,7 +4,7 @@ MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
 if [ "$MYIP" = "" ]; then
 	MYIP=$(wget -qO- ipv4.icanhazip.com)
 fi
-wget -q -O adit http://skrip.kerdunet.info/notfound.txt
+wget -q -O adit https://raw.githubusercontent.com/DomeiNokiO/xsped/master/adt.txt
 if ! grep -w -q $MYIP adit; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
 	if [[ $vps = "adityawg" ]]; then
