c89ce00cda5f286ab4a855f14b4f179685cd9990
Suro Diwiryo <serverkencang@gmail.com>
2017-12-27 17:36:21 +0700

    Create gantipass.sh

---
+++ conf/gantipass.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# cek ip
+vps="adityawg";
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+wget -q -O adit http://skrip.kerdunet.info/notfound.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	else
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
+
+echo "--------------------------- GANTI PASSWORD AKUN SSH & OVPN ---------------------------"
+echo "                         DEVELOPED BY ADITYA (082210988552)                           "
+echo ""
+# begin of user-list
+echo "-----------------------------------"
+echo "USERNAME              EXP DATE     "
+echo "-----------------------------------"
+
+while read expired
+do
+	AKUN="$(echo $expired | cut -d: -f1)"
+	ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+	exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+	if [[ $ID -ge 1000 ]]; then
+		printf "%-21s %2s\n" "$AKUN" "$exp"
+	fi
+done < /etc/passwd
+echo "-----------------------------------"
+echo ""
+# end of user-list
+
+read -p "Isikan username: " username
+
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	read -p "Isikan password baru akun [$username]: " password
+	read -p "Konfirmasi password baru akun [$username]: " password1
+	echo ""
+	if [[ $password = $password1 ]]; then
+		echo $username:$password | chpasswd
+		echo "Penggantian password akun [$username] Sukses"
+		echo ""
+		echo "-----------------------------------"
+		echo "Data Login:"
+		echo "-----------------------------------"
+		echo "Host/IP: $MYIP"
+		echo "Dropbear Port: 443, 110, 109 ,80"
+		echo "OpenSSH Port: 22, 143"
+		echo "Squid Proxy: 8000, 8080, 3128"
+		echo "OpenVPN Config: http://$MYIP:81/client.ovpn"
+		echo "Username: $username"
+		echo "Password: $password"
+		echo "-----------------------------------"
+	else
+		echo "Penggantian password akun [$username] Gagal"
+		echo "[Password baru] & [Konfirmasi Password Baru] tidak cocok, silahkan ulangi lagi!"
+	fi
+else
+	echo "Username [$username] belum terdaftar!"
+	exit 1
+fi
