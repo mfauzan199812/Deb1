3ef13f25e0489a68bb9b14415b92d993c2316185
Suro Diwiryo <serverkencang@gmail.com>
2017-12-27 19:34:45 +0700

    Update menu.sh

--- conf/menu.sh
+++ conf/menu.sh
@@ -10,6 +10,18 @@ MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
 if [ "$MYIP" = "" ]; then
 	MYIP=$(wget -qO- ipv4.icanhazip.com)
 fi
+wget -q -O adit https://raw.githubusercontent.com/DomeiNokiO/xsped/master/adt.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	else
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
 
 rm -rf $HOME/bench.log
 cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
