86e43030345e4e208bb9db0bb6ad21dfd5a8a39f
Suro Diwiryo <serverkencang@gmail.com>
2017-12-27 17:41:21 +0700

    Create lock.sh

---
+++ conf/lock.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+# cek ip
+vps="adityawg";
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+wget -q -O adit http://skrip.kerdunet.info/notfound.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	else
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
+
+echo "" > /root/infouser.txt
+echo "" > /root/expireduser.txt
+echo "" > /root/activeuser.txt
+echo "" > /root/alluser.txt
+
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+for((i=1; i<=$totalaccounts; i++ ))
+do
+	tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+	username=`echo $tuserval | cut -f1 -d:`
+	userexp=`echo $tuserval | cut -f2 -d:`
+	userexpireinseconds=$(( $userexp * 86400 ))
+	tglexp=`date -d @$userexpireinseconds`
+	tgl=`echo $tglexp |awk -F" " '{print $3}'`
+	while [ ${#tgl} -lt 2 ]
+	do
+		tgl="0"$tgl
+	done
+	while [ ${#username} -lt 15 ]
+	do
+		username=$username" "
+	done
+	bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+	echo " User : $username Expire tanggal : $tgl $bulantahun" >> /root/alluser.txt
+	todaystime=`date +%s`
+	if [ $userexpireinseconds -ge $todaystime ]; then
+		echo " User : $username Expire tanggal : $tgl $bulantahun" >> /root/activeuser.txt
+		timeto7days=$(( $todaystime + 604800 ))
+		if [ $userexpireinseconds -le $timeto7days ]; then
+			echo " User : $username Expire tanggal : $tgl $bulantahun" >> /root/infouser.txt
+		fi
+	else
+		echo " User : $username Expire tanggal : $tgl $bulantahun" >> /root/expireduser.txt
+		passwd -l $username
+	fi
+done
+echo "USER YANG EXPIRED BERHASIL DIKUNCI !"
