6ae1ddd032f895ba3f2d6deb4f8e532c09a2f93f
Suro Diwiryo <serverkencang@gmail.com>
2017-12-27 17:39:05 +0700

    Create hapus.sh

---
+++ conf/hapus.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# cek ip
+vps="adityawg";
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+wget -q -O adit http://skrip.kerdunet.info/notfound.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	else
+		echo "Hubungi: AdityaWg atau (082210988552)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
+
+MYIP=$(wget -qO- ipv4.icanhazip.com)
+
+echo "---------------------- HAPUS AKUN SSH & OVPN -----------------------"
+
+	echo "           DEVELOPED BY ADITYAWG - 082210988552            "
+echo ""
+
+# begin of user-list
+echo "-----------------------------------"
+echo "USERNAME              EXP DATE     "
+echo "-----------------------------------"
+
+while read expired
+do
+	AKUN="$(echo $expired | cut -d: -f1)"
+	ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+	exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+	if [[ $ID -ge 1000 ]]; then
+		printf "%-21s %2s\n" "$AKUN" "$exp"
+	fi
+done < /etc/passwd
+echo "-----------------------------------"
+echo ""
+# end of user-list
+
+read -p "Isikan username: " username
+
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	echo ""
+	read -p "Apakah Anda benar-benar ingin menghapus akun [$username] [y/n]: " -e -i y REMOVE
+	if [[ "$REMOVE" = 'y' ]]; then
+		passwd -l $username
+		userdel $username
+		echo ""
+		echo "Akun [$username] berhasil dihapus!"
+	else
+		echo ""
+		echo "Penghapusan akun [$username] dibatalkan!"
+	fi
+else
+	echo "Username [$username] belum terdaftar!"
+	exit 1
+fi
