a34edaca228ae33b6e61ff52a388e2aa92e6563d
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-04 16:37:31 +0000

    Update renew.sh

--- conf/renew.sh
+++ conf/renew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script By AdityaWg (adityawg@hotmail.com) (082210988552)
+#Script By Mohammad Fauzan R (fauzan121998@gmail.com) (0895703796928)
 if [[ $USER != 'root' ]]; then
 	echo "Maaf, Anda harus menjalankan ini sebagai root"
 	exit
@@ -14,16 +14,16 @@ wget -q -O adit https://raw.githubusercontent.com/mfauzan199812/Deb1/master/ip.t
 if ! grep -w -q $MYIP adit; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
 	if [[ $vps = "adityawg" ]]; then
-		echo "Hubungi: Moh Fauzan R atau (0895703796928)"
+		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
 	else
-		echo "Hubungi: Moh Fauzan R atau (0895703796928)"
+		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
 	fi
 	rm /root/adit
 	rm -f /root/adit
 	exit
 fi
 echo "--------------------PERPANJANG MASA AKTIF AKUN-------------------"
-echo "             DEVELOPED BY Moh Fauzan R (089570379692)            "
+echo "         DEVELOPED BY Mohammad Fauzan R (089570379692)            "
 echo ""
 
 # begin of user-list
