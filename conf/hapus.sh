a2b161d1defe11a7c099781c2d3a4548aa4c7ff1
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-04 21:35:15 +0700

    Update hapus.sh

--- conf/hapus.sh
+++ conf/hapus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Script By AdityaWg (adityawg@hotmail.com) (082210988552)
 if [[ $USER != 'root' ]]; then
 	echo "Maaf, Anda harus menjalankan ini sebagai root"
 	exit
@@ -11,13 +11,13 @@ MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
 if [ "$MYIP" = "" ]; then
 	MYIP=$(wget -qO- ipv4.icanhazip.com)
 fi
-wget -q -O adit https://raw.githubusercontent.com/DomeiNokiO/xsped/master/adt.txt
+wget -q -O adit https://raw.githubusercontent.com/mfauzan199812/Deb1/master/ip.txt
 if ! grep -w -q $MYIP adit; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
 	if [[ $vps = "adityawg" ]]; then
-		echo "Hubungi: AdityaWg atau (082210988552)"
+		echo "Hubungi: Moh Fauzan R atau (0895703796928)"
 	else
-		echo "Hubungi: AdityaWg atau (082210988552)"
+		echo "Hubungi: Moh Fauzan R atau (0895703796928)"
 	fi
 	rm /root/adit
 	rm -f /root/adit
@@ -27,7 +27,7 @@ MYIP=$(wget -qO- ipv4.icanhazip.com)
 
 echo "---------------------- HAPUS AKUN SSH & OVPN -----------------------"
 
-	echo "           DEVELOPED BY ADITYAWG - 082210988552            "
+	echo "           DEVELOPED BY Moh Fauzan R (089570379692)            "
 echo ""
 
 # begin of user-list
