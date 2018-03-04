7fe1a3e0c77c85900b1032ca4c1e89f4a00f8c37
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-04 21:14:51 +0700

    Update baned.sh

--- conf/baned.sh
+++ conf/baned.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Script By AdityaWg (adityawg@hotmail.com) (082210988552)
 clear
 if [[ $USER != 'root' ]]; then
 	echo "Maaf, Anda harus menjalankan ini sebagai root"
@@ -11,20 +12,20 @@ MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
 if [ "$MYIP" = "" ]; then
 	MYIP=$(wget -qO- ipv4.icanhazip.com)
 fi
-wget -q -O adit https://raw.githubusercontent.com/DomeiNokiO/xsped/master/adt.txt
+wget -q -O adit https://raw.githubusercontent.com/mfauzan199812/xsped/master/ip.txt
 if ! grep -w -q $MYIP adit; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
 	if [[ $vps = "adityawg" ]]; then
-		echo "Hubungi: AdityaWg atau (082210988552)"
+		echo "Hubungi: Moh Fauzan R atau (0895703796928)"
 	else
-		echo "Hubungi: AdityaWg atau (082210988552)"
+		echo "Hubungi: Moh Fauzan R atau (0895703796928)"
 	fi
 	rm /root/adit
 	rm -f /root/adit
 	exit
 fi
 echo -e "------------------ BANNED AKUN SSH & OVPN -------------------"
-echo -e "            DEVELOPED BY ADITYA (082210988552)               "
+echo -e "         DEVELOPED BY Moh Fauzan R (089570379692)            "
 # begin of user-list
 echo "-----------------------------------"
 echo "USERNAME              EXP DATE     "
