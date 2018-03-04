9267abb29c66762f1758409015f254ac7dd2299d
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-04 16:34:51 +0000

    Update rebotauto.sh

--- conf/rebotauto.sh
+++ conf/rebotauto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script By AdityaWg (adityawg@hotmail.com) (082210988552)
+#Script By Mohammad Fauzan R (fauzan121998@gmail.com) (0895703796928)
 tanggal=$(date +"%m-%d-%Y")
 waktu=$(date +"%T")
 echo "Eksekusi Reboot Server Berhasil Pada $tanggal Pukul $waktu >> /root/log-reboot.txt
