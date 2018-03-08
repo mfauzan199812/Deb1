90a020c0d22b1e2dd7c6e47c07bd1de85bd3e149
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-08 17:17:05 +0000

    Update menu.sh

--- conf/menu.sh
+++ conf/menu.sh
@@ -44,7 +44,7 @@ up=$( uptime | awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=
 	# Output of results
 echo -e "------------------ Selamat Datang Di Server Premium BogelSSH Team ----------------------"
 echo -e ""
-echo -e "--------------
+echo -e "--------------"
 echo -e "System Info"
 echo -e "--------------------------------------"
 echo -e "\e[032;1mServer Ip:\e[0m $MYIP"
