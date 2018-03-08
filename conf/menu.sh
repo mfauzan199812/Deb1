f6ff4b1a2edab7686034623a51db78e773b92b7d
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-08 10:31:22 +0000

    Update menu.sh

--- conf/menu.sh
+++ conf/menu.sh
@@ -42,7 +42,7 @@ tram=$( free -m | awk 'NR==2 {print $2}' )
 vram=$( free -m | awk 'NR==4 {print $2}' )
 up=$( uptime | awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }' | sed 's/^[ \t]*//;s/[ \t]*$//' )
 	# Output of results
-echo -e "----------------- Selamat Datang Di Server Premium BogelSSH Team ----------------------"
+echo -e "{red}------------------ Selamat Datang Di Server Premium BogelSSH Team ----------------------"
 echo -e "System Info"
 echo -e "-----------"
 echo -e "\e[032;1mServer Ip:\e[0m $MYIP"
@@ -56,7 +56,7 @@ echo -e "\e[032;1mScript By:\e[0m Mohammad Fauzan R (0895703796928) | BogelSSH T
 echo -e "-----------------------------------------------------------------------"
 echo -e ""
 echo -e "Apa Yang Ingin Anda Lakukan Admin?"
-echo -e ""
+echo -e "---------------------------
 PS3='Silahkan ketik angka pilihan anda lalu enter Ya Boss: '
 options=("Buat Akun SSH & OVPN" "Buat Akun Trial SSH & OVPN" "Generate Akun SSH & OVPN" "Perbarui Masa Aktif Akun" "Hapus Akun SSH & OVPN" "Ganti Password Akun SSH & OVPN" "Lihat Semua Akun SSH & OVPN" "Lihat Semua Akun Yang Expired" "Kunci Akun Yang Sudah Expired" "Hapus Akun Yang Sudah Expired" "Monitor Akun Yang Multi Login" "Tendang Akun Yang Multi Login" "Nyalakan Autokill" "Matikan Autokill" "Ram Status" "Penggunaan Data Oleh User" "Banned Akun SSH & OVPN" "Unbanned Akun SSH & OVPN" "Test Kecepatan Server" "Edit Baner Login" "Nyalakan Auto Reboot VPS 12 Jam Sekali" "Nyalakan Auto Reboot VPS 1 Hari Sekali" "Melihat Log Auto Reboot VPS" "Mematikan Auto Reboot VPS" "Ganti Password VPS" "Restart Server [reboot]" "Restart Webmin [service webmin restart]" "Restart Dropbear [service dropbear restart]" "Restart Squid [service squid restart]" "Quit")
 select opt in "${options[@]}"
