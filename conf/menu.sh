a0c89efe584c3538472b68fc11af084bf2c4fd07
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-11 17:47:57 +0000

    Update menu.sh

--- conf/menu.sh
+++ conf/menu.sh
@@ -44,7 +44,7 @@ up=$( uptime | awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=
 	# Output of result
 echo -e ""
 echo -e "${green}_________________ Selamat Datang Di Server Premium BogelSSH Team ______________________${NC}"
-echo -e "${red}______________________<<< Jangan Lupa Bahagia Ea Hari Ini >>>___________________________${NC}"
+echo -e "______________________<<< Jangan Lupa Bahagia Ea Hari Ini >>>___________________________"
 echo -e ""
 echo -e ""
 echo -e "-------------------"
@@ -60,9 +60,9 @@ echo -e "\e[032;1mSystem uptime:\e[0m $up"
 echo -e "\e[032;1mScript Mod By:\e[0m Mohammad Fauzan R (0895703796928) | BogelSSH TEAM"
 echo -e "-------------------------------------------------------------"
 echo -e ""
-echo -e "Apa Yang Ingin Anda Lakukan Sekarang Ini Sultan?"
+echo -e "${green}Apa Yang Ingin Anda Lakukan Sekarang Ini Sultan?${NC}"
 echo -e "__________________________________________"
-PS3='Silahkan Ketik Angka Pilihan Anda Lalu Enter Ya Boss: '
+PS3='${green}Silahkan Ketik Angka Pilihan Anda Lalu Enter Ya Boss:${NC} '
 options=("Buat Akun SSH & OVPN" "Buat Akun Trial SSH & OVPN" "Generate Akun SSH & OVPN" "Perbarui Masa Aktif Akun" "Hapus Akun SSH & OVPN" "Ganti Password Akun SSH & OVPN" "Lihat Semua Akun SSH & OVPN" "Lihat Semua Akun Yang Expired" "Kunci Akun Yang Sudah Expired" "Hapus Akun Yang Sudah Expired" "Monitor Akun Yang Multi Login" "Tendang Akun Yang Multi Login" "Nyalakan Autokill" "Matikan Autokill" "Ram Status" "Penggunaan Data Oleh User" "Banned Akun SSH & OVPN" "Unbanned Akun SSH & OVPN" "Test Kecepatan Server" "Edit Baner Login" "Nyalakan Auto Reboot VPS 12 Jam Sekali" "Nyalakan Auto Reboot VPS 1 Hari Sekali" "Melihat Log Auto Reboot VPS" "Mematikan Auto Reboot VPS" "Ganti Password VPS" "Restart Server [reboot]" "Restart Webmin [service webmin restart]" "Restart Dropbear [service dropbear restart]" "Restart Squid [service squid restart]" "Quit")
 select opt in "${options[@]}"
 do
