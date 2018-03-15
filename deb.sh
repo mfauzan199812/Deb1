4f08e3929c7d0550c3bfeb933a07c6226fda26c5
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-15 10:03:43 +0000

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-red='\e[1;31m'
-            green='\e[0;32m'
-            NC='\e[0m'
-            echo "Connecting to BogelSSH TEAM..."
-            sleep 0.2
-            echo "Checking Permision..."
-            sleep 0.3
-            echo -e "${green}Permission Accepted...${NC}"
-            sleep 1
-            echo""
+#Script By Mohammad Fauzan R (fauzan121998@gmail.com) (0895703796928)
 clear
 if [[ $USER != 'root' ]]; then
 	echo "Maaf, Anda harus menjalankan ini sebagai root"
 	exit
 fi
+
 # cek ip
 vps="adityawg";
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
