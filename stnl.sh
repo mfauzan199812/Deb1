9e22c13ed16526ed4667df728637b3162c688179
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-15 00:43:00 +0000

    Update stnl.sh

--- stnl.sh
+++ stnl.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+red='\e[1;31m'
+            green='\e[0;32m'
+            NC='\e[0m'
+            echo "Connecting to BogelSSH TEAM..."
+            sleep 0.2
+            echo "Checking Permision..."
+            sleep 0.3
+            echo -e "${green}Permission Accepted...${NC}"
+            sleep 1
+            echo""
+clear
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+# cek ip
+vps="adityawg";
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+wget -q -O adit https://raw.githubusercontent.com/mfauzan199812/Deb1/master/ip.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
+	else
+		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
+
 apt-get install stunnel4
 
 wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/DomeiNokiO/xsped/master/stunnel.conf"
