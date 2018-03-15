c326da2ccce413857e96ed96da30033ba63ff511
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-15 18:42:12 +0000

    Update stnl.sh

--- stnl.sh
+++ stnl.sh
@@ -37,13 +37,19 @@ email=fauzan121998@gmail.com
 
 #membuat sertifikat
 apt-get install stunnel4
+
 wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/mfauzan199812/xsped/master/stunnel.conf"
+
 openssl genrsa -out key.pem 2048
+
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
 -subj
 "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+
 /etc/init.d/stunnel4 restart
 
 #informasi
@@ -51,4 +57,4 @@ clear
 echo -e "__________________________"
 echo -e "Port SSL/TLS Default : 443"
 echo -e "By M Fauzan R (BogelSSH Team)"
-echo -e "___________________________
+echo -e "___________________________"
