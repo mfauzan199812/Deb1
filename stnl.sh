8a2af34cecbddfa82e120c88ae8241f3a45b9fe4
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-15 10:07:04 +0000

    Update stnl.sh

--- stnl.sh
+++ stnl.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+#Script By Mohammad Fauzan R (fauzan121998@gmail.com) (0895703796928)
+clear
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# cek ip
+vps="adityawg";
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+wget -q -O adit https://raw.githubusercontent.com/mfauzan199812/Deb1/master/ip.txt
+if ! grep -w -q $MYIP adit; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "adityawg" ]]; then
+		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
+	else
+		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
+	fi
+	rm /root/adit
+	rm -f /root/adit
+	exit
+fi
+
 apt-get install stunnel4
 
 wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/DomeiNokiO/xsped/master/stunnel.conf"
