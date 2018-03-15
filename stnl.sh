2e6c7d99232bcced0b27d8571baf276bfd1dc0f8
mfauzan199812 <37005574+mfauzan199812@users.noreply.github.com>
2018-03-15 18:29:40 +0000

    Update stnl.sh

--- stnl.sh
+++ stnl.sh
@@ -45,3 +45,10 @@ openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 /etc/init.d/stunnel4 restart
+
+#informasi
+clear
+echo -e "__________________________"
+echo -e "Port SSL/TLS Default : 443"
+echo -e "By M Fauzan R (BogelSSH Team)"
+echo -e "___________________________
