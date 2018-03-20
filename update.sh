d4c3ae8ea044cfe8b83dc2a6161c5962056af72a
Mohammad Romandhoni <37005574+mfauzan199812@users.noreply.github.com>
2018-03-20 08:34:38 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,21 +7,21 @@ if [[ $USER != 'root' ]]; then
 fi
 
 # cek ip
-vps="adityawg";
+vps="mfauzan";
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 if [ "$MYIP" = "" ]; then
 	MYIP=$(wget -qO- ipv4.icanhazip.com)
 fi
-wget -q -O adit https://raw.githubusercontent.com/mfauzan199812/Deb1/master/ip.txt
-if ! grep -w -q $MYIP adit; then
+wget -q -O mfauzan https://raw.githubusercontent.com/mfauzan199812/Deb1/master/ip.txt
+if ! grep -w -q $MYIP mfauzan; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
-	if [[ $vps = "adityawg" ]]; then
+	if [[ $vps = "mfauzan" ]]; then
 		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
 	else
 		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
 	fi
-	rm /root/adit
-	rm -f /root/adit
+	rm /root/mfauzan
+	rm -f /root/mfauzan
 	exit
 fi
 
