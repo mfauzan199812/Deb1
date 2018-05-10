7e0c9a98800c678e33319de6bbfcf1b3aa9617b4
Mohammad Romandhoni <37005574+mfauzan199812@users.noreply.github.com>
2018-05-10 11:21:49 +0000

    Update ocs.sh

--- ocs.sh
+++ ocs.sh
@@ -9,31 +9,7 @@ red='\e[1;31m'
             sleep 0.3
             echo -e "${green}Permission Accepted...${NC}"
             sleep 1
-            echo""
-clear
-if [[ $USER != 'root' ]]; then
-	echo "Maaf, Anda harus menjalankan ini sebagai root"
-	exit
-fi
-# cek ip
-vps="mfauzan";
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
-if [ "$MYIP" = "" ]; then
-	MYIP=$(wget -qO- ipv4.icanhazip.com)
-fi
-wget -q -O mfauzan https://raw.githubusercontent.com/mfauzan199812/Deb1/master/ip.txt
-if ! grep -w -q $MYIP mfauzan; then
-	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
-	if [[ $vps = "mfauzan" ]]; then
-		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
-	else
-		echo "Hubungi: Mohammad Fauzan R atau (0895703796928)"
-	fi
-	rm /root/mfauzan
-	rm -f /root/mfauzan
-	exit
-fi
-
+  
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
